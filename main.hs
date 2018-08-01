d8bfa064cecdbc4d10d3eb21882e17de3df72182
Kevin Ouyang <kevin_ouyang@brown.edu>
2018-08-01 10:33:58 -0400

    score, checkloss

--- main.hs
+++ main.hs
@@ -1,16 +1,16 @@
 module Main where
 
-import System.Random
+import System.Random as R
 import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game as Game
-
-{-# LANGUAGE ScopedTypeVariables #-}
+import Data.Set as Set
+import Data.Fixed
 
 
 -- type/data declarations and constants --
 
 type Snake = Path
-type GameState = (Snake, Point, Int, Direction)
+type GameState = (Snake, Point, Int, Direction, Direction)
 type Direction = Point
 
 newSnake :: Snake
@@ -34,19 +34,28 @@ background = black
 
 -- functions for rendering the picture --
 
-drawPicture :: GameState -> Picture
-drawPicture (snake, apple, _, _) =  Pictures  (color red (drawSquare apple) : (drawSnake snake))
+drawScore :: Int -> Picture
+drawScore score = color white (text $ show score)
 
-drawSquare :: Point -> Picture
-drawSquare bottomLeft@(x, y) =
+drawPicture :: GameState -> Picture
+drawPicture (snake, apple, score, _, _) =  Pictures  ((drawScore score): (drawSquare red apple) : (drawSnake white snake))
+
+drawSquare :: Color -> Point -> Picture
+drawSquare c bottomLeft@(x, y) =
     let topLeft = (x, y + 1)
         topRight = (x + 1, y + 1)
         bottomRight = (x + 1, y)
-    in polygon (map (\(x, y) -> (x * squareDim, y * squareDim)) [bottomLeft, topLeft, topRight, bottomRight])
+        coords = (Prelude.map (\(x, y) -> ((x-10) * squareDim, (y-10) * squareDim)) [bottomLeft, topLeft, topRight, bottomRight])
+    in color c (polygon coords)
 
-drawSnake :: Snake -> [Picture]
-drawSnake [] = []
-drawSnake snake@(x:xs) = color white (drawSquare x) : drawSnake xs
+drawSnake :: Color -> Snake -> [Picture]
+drawSnake _ [] = []
+drawSnake c snake@(x:xs) = (drawSquare c x) : drawSnake new_c xs
+    where new_c = changeColor (rgbaOfColor c)
+
+changeColor :: (Float, Float, Float, Float) -> Color
+changeColor (a, b, c, d) = new_c
+    where new_c = makeColor (a) (b - 0.1) (c - 0.1) (d)
 
 -- functions for handling game logic --
 
@@ -57,48 +66,58 @@ drawSnake snake@(x:xs) = color white (drawSquare x) : drawSnake xs
     -- where
         -- randPoint :: (Integer, Integer)
 	    -- randPoint = (fst (randomR (-20, 20) (mkStdGen 0)), fst (randomR (-20, 20) (mkStdGen 0)))
-		
-generateApple :: Snake -> Point
-generateApple snake
-    | let 
-	    randPoint :: (Integer, Integer)
-	    randPoint = (fst (randomR (-20, 20) (mkStdGen 0)), fst (randomR (-20, 20) (mkStdGen 0)))
-	  in (fromIntegral (fst randPoint), fromIntegral (snd randPoint)) `elem` snake = generateApple snake
-    | let 
-	    randPoint :: (Integer, Integer)
-	    randPoint = (fst (randomR (-20, 20) (mkStdGen 0)), fst (randomR (-20, 20) (mkStdGen 0)))
-	  in otherwise = (fromIntegral (fst randPoint), fromIntegral (snd randPoint))
-	    
+
+-- generateApple :: Snake -> Point
+-- generateApple snake
+    -- | let
+	--     randPoint :: (Integer, Integer)
+	--     randPoint = (fst (randomR (-20, 20) (mkStdGen 0)), fst (randomR (-20, 20) (mkStdGen 0)))
+	--   in (fromIntegral (fst randPoint), fromIntegral (snd randPoint)) `elem` snake = generateApple snake
+    -- | let
+	--     randPoint :: (Integer, Integer)
+	--     randPoint = (fst (randomR (-20, 20) (mkStdGen 0)), fst (randomR (-20, 20) (mkStdGen 0)))
+	--   in otherwise = (fromIntegral (fst randPoint), fromIntegral (snd randPoint))
+
+-- generateApple :: Point
+-- generateApple =
+--     let x = randomR(-20, 20)
+--         y = randomR(-20, 20)
+--     in (x, y)
+
+checkLoss :: Snake -> Bool
+checkLoss snake = length snake /= length snake_set
+    where snake_set = Set.fromList snake
 
 moveSnake :: Snake -> Direction -> Snake
-moveSnake snake (a, b) = (a + x, b + y) : init snake
+moveSnake snake (a, b) = (mod' (a + x) 20, mod' (b + y) 20) : init snake
     where (x, y) = head snake
-	
+
 growSnake :: Snake -> Direction -> Snake
-growSnake snake (a, b) = (a + x, b + y) : snake
+growSnake snake (a, b) = (mod' (a + x) 20, mod' (b + y) 20) : snake
 	where (x, y) = head snake
 
 handleKeyEvent :: Game.Event -> GameState -> GameState
-handleKeyEvent (EventKey k ks _ _) (snake, point, int, direction)
-    | SpecialKey KeyUp <- k, Down <- ks = (snake, point, int, (0, 1))
-    | SpecialKey KeyDown <- k, Down <- ks = (snake, point, int, (0, -1))
-    | SpecialKey KeyRight <- k, Down <- ks = (snake, point, int, (1, 0))
-    | SpecialKey KeyLeft <- k, Down <- ks = (snake, point, int, (-1, 0))
-    | otherwise = (snake, point, int, direction)
+handleKeyEvent (EventKey k ks _ _) (snake, point, int, direction, curr_direction)
+    | curr_direction /= (0, -1), SpecialKey KeyUp <- k, Down <- ks = (snake, point, int, (0, 1), curr_direction)
+    | curr_direction /= (0, 1), SpecialKey KeyDown <- k, Down <- ks = (snake, point, int, (0, -1), curr_direction)
+    | curr_direction /= (-1, 0), SpecialKey KeyRight <- k, Down <- ks = (snake, point, int, (1, 0), curr_direction)
+    | curr_direction /= (1, 0), SpecialKey KeyLeft <- k, Down <- ks = (snake, point, int, (-1, 0), curr_direction)
+    | otherwise = (snake, point, int, direction, curr_direction)
 handleKeyEvent _ game = game
 
 updateGame :: Float -> GameState -> GameState
-updateGame _ (snake, apple, score, direction@(a, b))
-	| (a + x, b + y) == apple = (growSnake snake direction, generateApple (growSnake snake direction), score, direction)
-	| otherwise = (moveSnake snake direction, apple, score, direction)
+updateGame _ (snake, apple, score, direction@(a, b), curr_direction)
+    | checkLoss snake = (snake, apple, score, direction, curr_direction)
+	| (a + x, b + y) == apple = (growSnake snake direction, apple, score + 1, direction, direction)
+	| otherwise = (moveSnake snake direction, apple, score, direction, direction)
 	where (x, y) = head snake
 
 -- run the game! --
 
 main :: IO ()
 main =
-    let newGame = (newSnake, (-1,-1), 0, (0, 1))
-        keyFrame = 4
+    let newGame = (newSnake, (0,0), 0, (0, 1), (0, 1))
+        keyFrame = 5
     in play window background keyFrame newGame drawPicture handleKeyEvent updateGame
 	-- let
 	    -- randPoint :: (Int, Int)
